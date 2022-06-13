3f57d8ee506d23fe602b1df9402a1182c0824e49
bazdu <bazdukovvalentin@gmail.com>
2022-06-13 14:46:46 +1000

    Add first test

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,4 +1,5 @@
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.Json;
 using JSON_Serializer;
 namespace Tests;
 
@@ -8,6 +9,10 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
-        
+        TestsClass testsClass = new TestsClass("this is string", 0, 'a', true);
+        string expected = JsonSerializer.Serialize(testsClass);
+        Serializer mySerializer = new Serializer();
+        string actual = mySerializer.Serialize(testsClass);
+        Assert.AreEqual(expected, actual);
     }
 }
