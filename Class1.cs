e3de78044d0d4efba7ff690029406b0f52142451
bazdu <bazdukovvalentin@gmail.com>
2022-07-04 01:11:53 +1000

    add deserialize

--- Class1.cs
+++ Class1.cs
@@ -1,22 +1,22 @@
-﻿using System.Reflection;
-using System.Text;
+﻿using System.Dynamic;
+using System.Reflection;
+using Microsoft.VisualStudio;
+using System.Linq;
 namespace JSON_Serializer;
 public class Serializer
 {
     
-    public string Serialize(object obj)
-    {
+    public string Serialize(object obj) {
         string jsonString = "";
         List<string> props = new List<string>();
         Type type = obj.GetType();
-        
+        jsonString += $"\"class\":{type}";
         foreach (PropertyInfo elem in type.GetProperties())
         {
             object? value = elem.GetValue(obj);
-            value =  value is char ? value.ToString() : value;
+            value = value is char ? value.ToString() : value;
             string textValue = value is string text ? $"\"{text.Replace("\"", "\\\"")}\"" : $"{value ?? "null"}";
             textValue = value is bool ? textValue.ToLower() : textValue;
-           
           string  currentPropertyFormat = $"\"{elem.Name}\":{textValue}";
             props.Add(currentPropertyFormat);
         }
@@ -32,8 +32,14 @@ public class Serializer
 
 
     object Deserialize(string s) {
-        dynamic obj = new object();
-        
+        s.Replace("{", "").Replace("}", "");
+        string[] fields = s.Split(',');
+        Dictionary<string, string> obj = new Dictionary<string, string>();
+        var k = new KeyValuePair<string, string>();
+        foreach (string field in fields) {
+            string[] fieldSeparator = field.Split(':');
+            obj.Add(fieldSeparator[0], fieldSeparator[1]);
+        }
         return obj;
     }
 }
