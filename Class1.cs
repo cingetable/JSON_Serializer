f16ddfbcf9374b27b0da44f6db940ea45da9ba4a
bazdu <bazdukovvalentin@gmail.com>
2022-07-03 11:37:27 +1000

    add empty desrialize method

--- Class1.cs
+++ Class1.cs
@@ -9,15 +9,15 @@ public class Serializer
         string jsonString = "";
         List<string> props = new List<string>();
         Type type = obj.GetType();
-    
+        
         foreach (PropertyInfo elem in type.GetProperties())
         {
             object? value = elem.GetValue(obj);
             value =  value is char ? value.ToString() : value;
             string textValue = value is string text ? $"\"{text.Replace("\"", "\\\"")}\"" : $"{value ?? "null"}";
-            
             textValue = value is bool ? textValue.ToLower() : textValue;
-            string currentPropertyFormat = $"\"{elem.Name}\":{textValue}";
+           
+          string  currentPropertyFormat = $"\"{elem.Name}\":{textValue}";
             props.Add(currentPropertyFormat);
         }
         for (int i = 0; i < props.Count; i++)
@@ -26,7 +26,14 @@ public class Serializer
             if (i != props.Count - 1)
                 jsonString += ",";
         }
-    
+    // {"fiels": ""}
         return "{" + jsonString + "}";
+    }
+
+
+    object Deserialize(string s) {
+        dynamic obj = new object();
+        
+        return obj;
     }
 }
